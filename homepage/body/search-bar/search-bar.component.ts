26ac4bad09900cd8e86b351c038b3691c402f8ef
misterimouto <johngod3@gmail.com>
2020-02-04 19:27:14 +1000

    aaaaaaaand i'm done, goodbye

--- homepage/body/search-bar/search-bar.component.ts
+++ homepage/body/search-bar/search-bar.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchBarComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+  }
 
   ngOnInit() {
   }
--- homepage/body/search-bar/search-bar.module.ts
+++ homepage/body/search-bar/search-bar.module.ts
@@ -1,21 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LogoComponent } from './logo/logo.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
-
+import { SearchBarComponent } from './search-bar.component';
 
 @NgModule({
   declarations: [
-    LogoComponent, 
     SearchBarComponent
   ],
   imports: [
     CommonModule
   ],
   exports: [
-    LogoComponent,
     SearchBarComponent
   ]
 })
-export class BodyModule { }
+export class SearchBarModule { }
